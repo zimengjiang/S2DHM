126c32deb5c88f2c6e3da6c68a0860d835331169
lxxue <lixxue@ethz.ch>
2020-07-11 11:38:35 +0200

    let cmu bind function overwrite argparse

--- s2dhm/robotcar.sh
+++ s2dhm/robotcar.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=1 python run.py \
 	--dataset robotcar \
 	--mode sparse_to_dense \
-	--out_fname ../results/robotcar/contrastive_only_top5.txt \
+	--out_fname ../results/robotcar/contrastive_only_intrinsics.txt \
 	--vgg_ckpt ../checkpoints/robotcar/ckpt0_only_contrastive_1e-7_marginpos01.pth.tar
